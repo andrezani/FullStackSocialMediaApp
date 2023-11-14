8db8e44341c000709e9eee00e18fadd13b47bcc1
Andrea <andrew.zani01@gmail.com>
2023-11-14 12:50:50 +0100

    Add routes for authentication and home page

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+
 import "./globals.css";
+import { Home } from "./_root/pages";
+import { SigninForm, SignupForm } from "./_auth/forms";
+import AuthLayout from "./_auth/AuthLayout";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello World!</h1>
-    </div>
+    <main className="flex h-screen">
+      <Routes>
+        {/* PUBLIC ROUTES */}
+        <Route element={AuthLayout}>
+          <Route path="/sign-in" element={<SigninForm />} />
+          <Route path="/sign-up" element={<SignupForm />} />
+        </Route>
+
+        {/* PRIVATE ROUTES */}
+        <Route index element={<Home />} />
+      </Routes>
+    </main>
   );
 };
 
