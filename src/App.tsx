5ed1440622139e03923cc19716703d83bd95263c
Andrea <andrew.zani01@gmail.com>
2023-11-15 19:54:02 +0100

    Refactor routing and layout components

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,31 @@
-import React from "react";
 import { Routes, Route } from "react-router-dom";
 
-import "./globals.css";
-import { Home } from "./_root/pages";
-import { SigninForm, SignupForm } from "./_auth/forms";
+import { Home } from "@/_root/pages";
 import AuthLayout from "./_auth/AuthLayout";
+import RootLayout from "./_root/RootLayout";
+import SignupForm from "@/_auth/forms/SignupForm";
+import SigninForm from "@/_auth/forms/SigninForm";
+import { Toaster } from "@/components/ui/toaster";
+
+import "./globals.css";
 
 const App = () => {
   return (
     <main className="flex h-screen">
       <Routes>
-        {/* PUBLIC ROUTES */}
-        <Route element={AuthLayout}>
+        {/* public routes */}
+        <Route element={<AuthLayout />}>
           <Route path="/sign-in" element={<SigninForm />} />
           <Route path="/sign-up" element={<SignupForm />} />
         </Route>
 
-        {/* PRIVATE ROUTES */}
-        <Route index element={<Home />} />
+        {/* private routes */}
+        <Route element={<RootLayout />}>
+          <Route index element={<Home />} />
+        </Route>
       </Routes>
+
+      <Toaster />
     </main>
   );
 };
