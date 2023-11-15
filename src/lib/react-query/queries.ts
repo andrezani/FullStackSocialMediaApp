ca8f4d9bf941b053785349683ef72abb9154ebc4
Andrea <andrew.zani01@gmail.com>
2023-11-15 18:54:19 +0100

    Add auth and user queries to react-query

---
+++ src/lib/react-query/queries.ts
@@ -0,0 +1,30 @@
+import { useMutation } from "@tanstack/react-query";
+import {
+  createUserAccount,
+  signInAccount,
+  signOutAccount,
+} from "@/lib/appwrite/api";
+import { INewUser } from "@/types";
+
+// ============================================================
+// AUTH QUERIES
+// ============================================================
+
+export const useCreateUserAccount = () => {
+  return useMutation({
+    mutationFn: (user: INewUser) => createUserAccount(user),
+  });
+};
+
+export const useSignInAccount = () => {
+  return useMutation({
+    mutationFn: (user: { email: string; password: string }) =>
+      signInAccount(user),
+  });
+};
+
+export const useSignOutAccount = () => {
+  return useMutation({
+    mutationFn: signOutAccount,
+  });
+};
