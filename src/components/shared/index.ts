86c5bf6e22b0e6f4b16e185f40f071622e00c3cd
Andrea <andrew.zani01@gmail.com>
2023-11-17 21:49:46 +0100

    Add PostStats component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -2,4 +2,5 @@ export { default as Bottombar } from "./Bottombar";
 export { default as LeftSidebar } from "./LeftSidebar";
 export { default as Loader } from "./Loader";
 export { default as PostCard } from "./PostCard";
+export { default as PostStats } from "./PostStats";
 export { default as FileUploader } from "./FileUploader";
