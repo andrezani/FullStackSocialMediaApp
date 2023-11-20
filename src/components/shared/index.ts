57e1268f967f1d2dc9aa0a0b87a8905e8cd436d7
Andrea <andrew.zani01@gmail.com>
2023-11-20 17:25:58 +0100

    Add GridPostList component

--- src/components/shared/index.ts
+++ src/components/shared/index.ts
@@ -3,4 +3,5 @@ export { default as LeftSidebar } from "./LeftSidebar";
 export { default as Loader } from "./Loader";
 export { default as PostCard } from "./PostCard";
 export { default as PostStats } from "./PostStats";
+export { default as GridPostList } from "./GridPostList";
 export { default as FileUploader } from "./FileUploader";
