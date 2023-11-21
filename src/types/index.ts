b61d78ea9574730a0c0577f4a1a1dae166a80615
Andrea <andrew.zani01@gmail.com>
2023-11-21 15:49:58 +0100

    Add new types for user and post updates

--- src/types/index.ts
+++ src/types/index.ts
@@ -4,6 +4,33 @@ export type INavLink = {
   label: string;
 };
 
+export type IUpdateUser = {
+  userId: string;
+  name: string;
+  bio: string;
+  imageId: string;
+  imageUrl: URL | string;
+  file: File[];
+};
+
+export type INewPost = {
+  userId: string;
+  caption: string;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
+export type IUpdatePost = {
+  postId: string;
+  caption: string;
+  imageId: string;
+  imageUrl: URL;
+  file: File[];
+  location?: string;
+  tags?: string;
+};
+
 export type IUser = {
   id: string;
   name: string;
