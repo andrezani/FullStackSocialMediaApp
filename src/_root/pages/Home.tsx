35e6f27de8c6792a9d534080e25e44b125ab1c82
Andrea <andrew.zani01@gmail.com>
2023-11-21 15:32:12 +0100

    Add UserCard component and fetch top creators

--- src/_root/pages/Home.tsx
+++ src/_root/pages/Home.tsx
@@ -1,8 +1,8 @@
 import { Models } from "appwrite";
 
 // import { useToast } from "@/components/ui/use-toast";
-import { Loader, PostCard } from "@/components/shared";
-import { useGetRecentPosts } from "@/lib/react-query/queries";
+import { Loader, PostCard, UserCard } from "@/components/shared";
+import { useGetRecentPosts, useGetUsers } from "@/lib/react-query/queries";
 
 const Home = () => {
   // const { toast } = useToast();
@@ -12,8 +12,13 @@ const Home = () => {
     isLoading: isPostLoading,
     isError: isErrorPosts,
   } = useGetRecentPosts();
+  const {
+    data: creators,
+    isLoading: isUserLoading,
+    isError: isErrorCreators,
+  } = useGetUsers(10);
 
-  if (isErrorPosts) {
+  if (isErrorPosts || isErrorCreators) {
     return (
       <div className="flex flex-1">
         <div className="home-container">
@@ -43,6 +48,21 @@ const Home = () => {
             </ul>
           )}
         </div>
+      </div>
+
+      <div className="home-creators">
+        <h3 className="h3-bold text-light-1">Top Creators</h3>
+        {isUserLoading && !creators ? (
+          <Loader />
+        ) : (
+          <ul className="grid 2xl:grid-cols-2 gap-6">
+            {creators?.documents.map((creator) => (
+              <li key={creator?.$id}>
+                <UserCard user={creator} />
+              </li>
+            ))}
+          </ul>
+        )}
       </div>
     </div>
   );
